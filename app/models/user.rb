b86a3e47e777268535ebbd939381d35fd29c9b70
Thierry Chau <thierry.chau@gmail.com>
2023-10-12 22:19:54 +0000

    created sample_data

--- app/models/user.rb
+++ app/models/user.rb
@@ -32,7 +32,7 @@ class User < ApplicationRecord
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
-  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest", dependent: :destroy
   has_many :accepted_sent_follow_requests, -> { accepted }, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :accepted_received_follow_requests, -> { accepted }, foreign_key: :recipient_id, class_name: "FollowRequest"
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,3 +1,24 @@
 desc "Fill the database tables with some sample data"
 task({ :sample_data => :environment }) do
+  p "Creating sample data"
+  
+  if Rails.env.development?
+    FollowRequest.destroy_all
+    Comment.destroy_all
+    Like.destroy_all
+    Photo.destroy_all
+    User.destroy_all
+  do
+
+  12.times do
+    name = Faker::Name.first_name
+    User.create(
+      email: "#{name}@example.com",
+      password: "password",
+      username: name,
+      private: [true, false].sample,
+    )
+  end
+
+  p "There are now #{User.count} users."
 end
