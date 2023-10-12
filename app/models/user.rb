567c2389b58a03eedbe785e99142b78ec15f61b1
Thierry Chau <thierry.chau@gmail.com>
2023-10-12 21:15:41 +0000

    Adding all other association accessors

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,4 +30,7 @@ class User < ApplicationRecord
 
   has_many :own_photos, class_name: "Photo", foreign_key: "owner_id"
   has_many :comments, foreign_key: "author_id"
+  has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
+  has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
+  has_many :likes, foreign_key: :fan_id
 end
