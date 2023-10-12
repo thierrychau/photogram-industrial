20620cc7b18a878614d8403b707387f90768e4cf
Thierry Chau <thierry.chau@gmail.com>
2023-10-12 22:03:53 +0000

    added scopes to photo model

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -22,9 +22,12 @@
 class Photo < ApplicationRecord
   validates :caption, presence: true
   validates :image, presence: true
-  
+
   belongs_to :owner, class_name: "User", counter_cache: true
   has_many :comments
   has_many :likes
   has_many :fans, through: :likes
+  
+  scope :past_week, -> { where(created_at: 1.week.ago...) }
+  scope :by_likes, -> { order(likes_count: :desc) }
 end
