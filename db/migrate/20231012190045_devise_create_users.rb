dfdbc857118e73398cf89e280465c423908e717b
Thierry Chau <thierry.chau@gmail.com>
2023-10-12 20:15:58 +0000

    migrated user database

--- db/migrate/20231012190045_devise_create_users.rb
+++ db/migrate/20231012190045_devise_create_users.rb
@@ -3,8 +3,9 @@
 class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
+      enable_extension("citext")
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.citext :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -32,10 +33,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :username
+      t.citext :username
       t.boolean :private
-      t.integer :likes_count
-      t.integer :comments_count
+      t.integer :likes_count, default: 0
+      t.integer :comments_count, default: 0
 
       t.timestamps null: false
     end
@@ -44,5 +45,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
+    add_index :users, :username,             unique: true
   end
 end
