cb0182bee8fd26539a3a6c038b00ba53c5b50203
Thierry Chau <thierry.chau@gmail.com>
2023-10-12 21:27:54 +0000

    added indirect association accessor

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,5 @@ class User < ApplicationRecord
   has_many :sent_follow_requests, foreign_key: :sender_id, class_name: "FollowRequest"
   has_many :received_follow_requests, foreign_key: :recipient_id, class_name: "FollowRequest"
   has_many :likes, foreign_key: :fan_id
+  has_many :liked_photos, through: :likes, source: :photos
 end
