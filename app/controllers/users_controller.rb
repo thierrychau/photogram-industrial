6b74667f8340d7c8574ebcc1349795adea3b22ec
Thierry Chau <thierry.chau@gmail.com>
2023-10-16 15:06:50 +0000

    added followers and following routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :liked, :feed]
+  before_action :set_user, only: [:show, :liked, :feed, :followers, :following]
 
   def show
   end
@@ -8,6 +8,12 @@ class UsersController < ApplicationController
   end
 
   def feed
+  end
+
+  def followers
+  end
+
+  def following
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   get ":username/liked" => "users#liked", as: :liked
   get ":username/feed" => "users#feed", as: :feed
-  # get ":username/followers"
-  # get ":username/following"
+  get ":username/followers" => "users#followers", as: :followers
+  get ":username/following" => "users#following", as: :following
   get ":username" => "users#show", as: :user
 end
